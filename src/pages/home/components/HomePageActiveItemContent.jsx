6318f1e62ab570e9da816f8546b6ee3c9165f2d5
Sean Dorr <sdorr@Gemmas-iMac.local>
2020-10-17 01:15:48 +0200

    chore: adding propTypes style: adding dynamic styles for each item chore: only rendering item if active

--- src/pages/home/components/HomePageActiveItemContent.jsx
+++ src/pages/home/components/HomePageActiveItemContent.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
 import PropTypes from "prop-types";
-import homePageItems from "../consts/homePageItems";
+import colors from "../../../colors.scss";
 
-const HomePageActiveItemContent = ({ activeItem }) => {
-  return (
-    <div>
+const HomePageActiveItemContent = ({ homePageItems, activeItem, value, color }) => {
+  return homePageItems[activeItem].value === value ? (
+    <div 
+      className="spotify-playlist" 
+      css={css`
+        border: 20px solid ${colors[color]}
+      `}>
       <iframe
         title="spotify-playlist"
         src={`https://open.spotify.com/embed?uri=spotify:user:122367656:playlist:${homePageItems[activeItem].playlistURL}`}
@@ -15,9 +20,20 @@ const HomePageActiveItemContent = ({ activeItem }) => {
         allow="encrypted-media"
       />
     </div>
-  );
+  ) : null;
 };
 
-HomePageActiveItemContent.propTypes = {};
+HomePageActiveItemContent.propTypes = {
+  homePageItems: PropTypes.objectOf(Object).isRequired,
+  activeItem: PropTypes.string,
+  value: PropTypes.string,
+  color: PropTypes.string,
+};
+
+HomePageActiveItemContent.defaultProps = {
+  activeItem: undefined,
+  value: undefined,
+  color: undefined,
+};
 
 export default HomePageActiveItemContent;
